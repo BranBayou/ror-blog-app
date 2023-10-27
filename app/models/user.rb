bad0f4c288b85ae1cc0f385f1740b4a0b322382e
BranBayou <berhan.baye@gmail.com>
2023-10-27 18:14:19 +0300

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
@@ -7,5 +6,4 @@ class User < ApplicationRecord
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
-
 end
