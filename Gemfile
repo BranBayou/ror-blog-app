c9f1d49e1295f3663e2b17581d92446d297d325d
BranBayou <berhan.baye@gmail.com>
2023-10-28 23:04:24 +0300

    Add rspec for model and validation

--- Gemfile
+++ Gemfile
@@ -51,11 +51,6 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
-  gem 'rspec-rails'
-end
-
-group :test do
-  gem "rspec-rails"
 end
 
 group :development do
@@ -72,6 +67,8 @@ end
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem 'capybara'
+  gem 'rspec-rails'
   gem 'selenium-webdriver'
+  gem 'shoulda-matchers'
 end
 gem 'rubocop', '>= 1.0', '< 2.0'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'testing user with no values' do
+    subject { User.new }
+    before { subject.save }
+
+    it 'require user to have name' do
+      expect(subject).to_not be_valid
+    end
+  end
+  context 'testing user with values' do
+    subject(:user) { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0) }
+    before { user.save }
+
+    it 'user with name should be valid' do
+      expect(user).to be_valid
+    end
+
+    it 'posts counter should be equal to 0' do
+      expect(user.posts_counter).to eq(0)
+    end
+  end
 end
