a56ceb0025b0336ad8b34ae3a3ffd1ef5cd8227a
BranBayou <berhan.baye@gmail.com>
2023-11-04 14:52:06 +0300

    Add methods to models

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
 
   root "users#index"
-  resources :users, only: %i[show index]
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  resources :users, only: [:show] do
+    resources :posts, only: [:index, :show]
+  end
+
+  resources :posts, only: [:new, :create] do
+    resources :comments, only: [:new, :create]
+    resources :likes, only: [:create]
+  end
+
+
+
+  # resources :users, only: %i[show index]
+  # get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  # get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
