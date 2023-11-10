8f0ba883557f31748ebcb4fd681172e383fc1797
BranBayou <berhan.baye@gmail.com>
2023-11-10 18:21:40 +0300

    Add style tologin forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations'
+      }
 
   root "users#index"
   resources :users, only: [:index, :show] do
